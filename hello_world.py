554b496291e520702f67d5c2257a7dba8dded40b
Thelina Nguyen <bbaotramm2911@gmail.com>
2024-07-22 18:04:24 +0300

    fixed merge conflicts

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,5 @@
 print("Hello world!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("I've come to talk with you again")
-=======
 print("Hello darkness my old friend")
->>>>>>> merge-mat/master
-=======
+print("I've come to talk with you again")
 
->>>>>>> 750057fdff51aeda1237c076809f8b76f1a743d3
+
